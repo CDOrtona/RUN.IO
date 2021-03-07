1fdfc12aeed590d2ad91e31b737372d0a4d53eec
Durton24 <github@cdmails.anonaddy.com>
2021-03-07 19:50:28 +0100

    RSSI Graph fixed

--- app/src/main/java/org/cdortona/tesi/GraphRssi.java
+++ app/src/main/java/org/cdortona/tesi/GraphRssi.java
@@ -11,14 +11,22 @@ import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.ViewParent;
 
+import com.jjoe64.graphview.CursorMode;
+import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
 import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.text.NumberFormat;
 
 /**
  * Cristian D'Ortona
@@ -37,12 +45,12 @@ public class GraphRssi extends AppCompatActivity {
 
     //Graph
     LineGraphSeries<DataPoint> series;
-    int xAxis = 0;
+    double xAxis = 0d;
+    Handler mHandler;
 
     //BLE
     BluetoothManager manager;
     BluetoothAdapter adapter;
-    BluetoothDevice deviceToGetRssiFrom;
     //I need to instantiate a scanner object which I'll use to start a new scan
     //The phone will constantly scan for nearby devices with the sole purpose of gathering RSSI info of the selected device
     BluetoothLeScanner bluetoothLeScanner;
@@ -59,31 +67,49 @@ public class GraphRssi extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //Graph
+        mHandler = new Handler();
         GraphView graph = findViewById(R.id.graph);
         series = new LineGraphSeries<>();
+        series.setAnimated(true);
+        series.setDrawBackground(true);
+        series.setColor(Color.BLUE);
+        series.setBackgroundColor(R.color.design_default_color_primary);
         graph.addSeries(series);
+
+        GridLabelRenderer gridLabelRenderer = graph.getGridLabelRenderer();
+        gridLabelRenderer.setHighlightZeroLines(false);
+        gridLabelRenderer.setVerticalAxisTitle("RSSI(dBm)");
+        gridLabelRenderer.setVerticalAxisTitleColor(Color.BLACK);
+        gridLabelRenderer.setVerticalLabelsColor(R.color.lightPrimaryColor);
+        gridLabelRenderer.setHorizontalLabelsColor(R.color.lightPrimaryColor);
+        gridLabelRenderer.setNumVerticalLabels(10);
+        gridLabelRenderer.setNumHorizontalLabels(6);
+        gridLabelRenderer.setPadding(35);
+        gridLabelRenderer.setGridStyle(GridLabelRenderer.GridStyle.BOTH);
+
         Viewport viewport = graph.getViewport();
-        viewport.setYAxisBoundsManual(true);
-        viewport.setMinX(0);
-        viewport.setMinY(0);
         viewport.setScrollable(true);
+        viewport.setScalable(true);
+        viewport.setXAxisBoundsManual(true);
+        viewport.setMinX(0);
+        viewport.setMaxX(10);
+        viewport.setMinY(0);
 
         //BLE
         manager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         adapter = manager.getAdapter();
         Intent intent = getIntent();
-        deviceToGetRssiFrom = adapter.getRemoteDevice(intent.getStringExtra(StaticResources.EXTRA_CHOOSEN_ADDRESS));
+        deviceAddress = intent.getStringExtra(StaticResources.EXTRA_CHOOSEN_ADDRESS);
         bluetoothLeScanner = adapter.getBluetoothLeScanner();
         bluetoothLeScanner.startScan(leScanCallBack);
     }
-
 
     //callBack for the scanning process
     ScanCallback leScanCallBack = new ScanCallback() {
         @Override
         public void onScanResult(int callbackType, ScanResult result) {
             super.onScanResult(callbackType, result);
-            valueToGraph(result);
+            valueToGraph(new DevicesScannedModel(deviceAddress, result.getRssi()));
         }
 
         @Override
@@ -93,22 +119,24 @@ public class GraphRssi extends AppCompatActivity {
         }
     };
 
-    void valueToGraph(ScanResult result){
-        /*try {
-            Thread.sleep(500);
-        } catch (InterruptedException e){
-            e.printStackTrace();
-        }*/
-        Log.d(TAG, "Result from scan: " + result.getDevice().getAddress() + ", chosen address: " + deviceAddress);
-        if(result.getDevice().getAddress().equals(deviceAddress)){
-            Log.d(TAG, "Device found: " + result.getDevice().getName() + ", updating RSSI");
-            series.appendData(new DataPoint(xAxis,result.getRssi()), true, 20);
+    void valueToGraph(final DevicesScannedModel scannedDevice){
+        Log.d(TAG, "Result from scan: " + scannedDevice.getBleAddress() + ", chosen address: " + deviceAddress);
+        if(scannedDevice.getBleAddress().equals(deviceAddress)) {
+            Log.d(TAG, "Device found: " + scannedDevice.getBleAddress() + ", updating RSSI");
+            //anonymous inner class with interface, the anonymous class implements the abstract class Runnable and has to override the abstract method run
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    xAxis += 1d;
+                    series.appendData(new DataPoint(xAxis, scannedDevice.getRssi()), true, 100);
+                }
+            }, 2500);
         }
     }
 
     //Toolbar
     public boolean onSupportNavigateUp() {
-        //this is called when the activity detects the user pressed the back button
+        //this is called when the activity detects the user pressed the up button in the toolbar
         onBackPressed();
         return true;
     }
