60630121486c6bfbaa638cc8b4121d72477aca52
Durton24 <github@cdmails.anonaddy.com>
2021-03-10 18:43:45 +0100

    rebuilt GATT client

--- app/src/main/java/org/cdortona/tesi/StaticResources.java
+++ app/src/main/java/org/cdortona/tesi/StaticResources.java
@@ -16,20 +16,22 @@ class StaticResources {
     static final String ESP32_SERVICE = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
     static final String ESP32_TEMP_CHARACTERISTIC = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
     static final String ESP32_HEARTH_CHARACTERISTIC = "5ebad8b8-8128-11eb-8dcd-0242ac130003";
+    static final String ESP32_BRIGHTNESS_CHARACTERISTIC = "3935a44c-81c3-11eb-8dcd-0242ac130003";
     //client characteristic configuration which is used to by the client in order to configure the
     //Indicate or Notify property of that specified characteristic
     static final String ESP32_DESCRIPTOR = "00002902-0000-1000-8000-00805f9b34fb";
 
     //Broadcaster Intent Actions what will be received by the broadcast receiver
-    static final String BROADCAST_CONNECTION_STATE = PACKAGE_NAME + ".connectionState";
-    static final String BROADCAST_ESP32_INFO =  PACKAGE_NAME + ".serviceFound";
-    static final String BROADCAST_CHARACTERISTIC_CHANGED = PACKAGE_NAME + ".characteristicChanged";
-    static final String BROADCAST_CHARACTERISTIC_READ = PACKAGE_NAME + ".characteristicRead";
+    static final String ACTION_CONNECTION_STATE = PACKAGE_NAME + ".connectionState";
+    static final String ACTION_CHARACTERISTIC_CHANGED = PACKAGE_NAME + ".characteristicChanged";
+    static final String ACTION_CHARACTERISTIC_CHANGED_READ = PACKAGE_NAME + "characteristicChangedRead";
 
     //Keys for the putExtra method
     static final String EXTRA_STATE_CONNECTION = "keyConnection";
-    static final String EXTRA_CHARACTERISTIC_VALUE_READ = "charReadValue";
-    static final String EXTRA_CHARACTERISTIC_NOTIFIED = "characteristicToBeNotified";
+    static final String EXTRA_TEMP_VALUE = "tempValue";
+    static final String EXTRA_HEART_VALUE = "heartValue";
+    static final String EXTRA_BRIGHTNESS_VALUE = "brightnessValue";
+    static final String EXTRA_CHARACTERISTIC_CHANGED = "characteristicToBeNotified";
 
     //values for the putExtra method
     static final String STATE_CONNECTED = "Connected";
