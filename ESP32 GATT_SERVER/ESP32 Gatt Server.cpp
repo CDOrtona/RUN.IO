fbba8eacc832a2829e621d507a23f089559908f1
CDOrtona <github@cdmails.anonaddy.com>
2021-04-06 01:20:44 +0200

    minor fix

--- ESP32 GATT_SERVER/ESP32 Gatt Server.cpp
+++ ESP32 GATT_SERVER/ESP32 Gatt Server.cpp
@@ -117,6 +117,8 @@ void loop() {
           delay(1000); // bluetooth stack will go into congestion, if too many packets are sent
         }
     }
+
+    //this is used in order to make the ESP32 advertise again once it disconnects from the Central 
     // disconnecting
     if (!deviceConnected && oldDeviceConnected) {
         delay(500);
